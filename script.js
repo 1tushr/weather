414f1ba994088a1d89c33e6f7255dba3f8ab4717
Tushar Verma <tuhsar2022@gmail.com>
2023-04-22 23:48:36 +0530

    weather

--- script.js
+++ script.js
@@ -38,31 +38,55 @@ fetchDataButton.addEventListener("click", () => {
         alert("Map already created!");
       }
 
-      // use OpenWeatherMap API to get weather data
-      fetch(`https://api.openweathermap.org/data/3.0/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=711c4d78eb670d25b9ea7c1e2244d9bb`)
-        .then(response => response.json())
-        .then(data => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=711c4d78eb670d25b9ea7c1e2244d9bb`
+      )
+        .then((response) => response.json())
+        .then((data) => {
           const weatherDiv = document.getElementById("weather");
 
-          
-          const temperature = data.current.temp;
-          const weatherDescription = data.current.weather[0].description;
-          const humidity = data.current.humidity;
-          const windSpeed = data.current.wind_speed;
+          // extract weather data
+          const locationName = data.name;
+          const TimeZone=data.timezone;
+          const temperature = data.main.temp;
+          const weatherDescription = data.weather[0].description;
+          const humidity = data.main.humidity;
+          const windSpeed = data.wind.speed;
+          const pressure = data.main.pressure;
+          const windDirection = data.wind.deg;
+            const uvi = data.visibility;
+            const feels = data.main.feels_like;
+
+
+          // update HTML with weather data
           const weatherHTML = `
-            <h2>Weather Information:</h2>
+          <div id="info">
+            <h2 id="details">Weather Data</h2>
+            <div id="list">
             <ul>
+              <li>Location: ${locationName}</li>
+              <li>Lat: ${latitude}  <span> Long: ${longitude}</span>
+              <li>TimeZone: ${TimeZone}</li>
+              <li>Wind Speed: ${windSpeed} m/s</li>
+              <li>Pressure: ${pressure} hPa</li>
+              <li>Humidity: ${humidity}%</li>
+              <li>Wind Direction: ${windDirection}&deg</li>
+              <li>UV Index: ${uvi} </li>
+              <li> Feels Like: ${feels} </li>
               <li>Temperature: ${temperature} &deg;C</li>
               <li>Weather Description: ${weatherDescription}</li>
-              <li>Humidity: ${humidity}%</li>
-              <li>Wind Speed: ${windSpeed} km/h</li>
+             
+             
+             
             </ul>
+            </div>
+            <div></div>
+            </div>
           `;
 
-          
           weatherDiv.innerHTML = weatherHTML;
         })
-        .catch(error => console.error(error));
+        .catch((error) => console.error(error));
     },
     () => {
       alert("Unable to retrieve your location");
